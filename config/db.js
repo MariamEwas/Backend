ce8e566e68dfe0640c5716f5189177ae42096947
MariamEwas <150853335+MariamEwas@users.noreply.github.com>
2024-09-14 00:04:20 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/Portfolio');
+        await mongoose.connect('mongodb+srv://maryemhasan2004:Mar_582004@cluster0.icojq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Connected to mvc1 database 1');
     }
     catch(err){
